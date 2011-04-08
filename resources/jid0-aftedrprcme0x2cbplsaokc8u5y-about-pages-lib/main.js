c9021809507a31a0e60f008aa15d1976267e64cf
Soumya Deb <debloper@gmail.com>
2011-04-08 19:41:58 +0530

    Fully working version to be released on AMO.

--- resources/jid0-aftedrprcme0x2cbplsaokc8u5y-about-pages-lib/main.js
+++ resources/jid0-aftedrprcme0x2cbplsaokc8u5y-about-pages-lib/main.js
@@ -1,15 +1,24 @@
-// This is an active module of the about:pages Add-on
-// https://builder.mozillalabs.com/api/jetpack-core/module/context-menu/
+// This is the active module of the About Pages Add-on
+// Original Author: Debloper
+
 var contextMenu = require("context-menu");
 var tabs = require("tabs");
 
-var aboutContextMenu = contextMenu.Item({
-  label: "about:pages",
-  data : "about:about",
+var aboutContextMenu = contextMenu.Menu({
+  label: "About Pages",
   contentScript: 'on("click", function (node, data) {' +
   '  postMessage(data.toString());' +
   '});',
   onMessage: function(url) {
       tabs.open(url);
-  }
+  },
+  items: [
+      contextMenu.Item({ label: "About:Home", data: "about:home" }),
+      contextMenu.Item({ label: "About:Addons", data: "about:addons" }),
+      contextMenu.Item({ label: "About:Plugins", data: "about:plugins" }),
+      contextMenu.Item({ label: "About:Config", data: "about:config" }),
+      contextMenu.Item({ label: "About:Support", data: "about:support" }),
+      contextMenu.Separator(),
+      contextMenu.Item({ label: "All Abouts", data: "about:about" })
+  ]
 });
